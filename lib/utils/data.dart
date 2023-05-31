f11271ae9e4b77de4823021bbf7c25a508f13fdc
Deepak Yadav <dy22900@gmail.com>
2023-05-31 14:54:19 +0530

    api changes

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,10 +1,10 @@
 
 import '../Models/movie_model.dart';
 
-const String apikey = '17489671240e5bd9423d6c7c7a8566d0';
+const String apikey = 'Add your api key here';
 
 const readaccesstoken =
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzQ4OTY3MTI0MGU1YmQ5NDIzZDZjN2M3YTg1NjZkMCIsInN1YiI6IjY0NzVkY2UyOTYzODY0MDBjMWYzZjIxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-JKf3kHxgWMzk2NmNt9Gb0g41Tyb_5p7Tcxgc5pi44o';
+    'Add your readaccesstoken here';
 
 final List favoriteMovies = [];
 
